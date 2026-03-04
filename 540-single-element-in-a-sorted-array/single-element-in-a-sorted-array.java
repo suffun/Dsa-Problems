a725a28fe1dce81248af937a7febf5512c6140e8
sufiyan khan <160502499+suffun@users.noreply.github.com>
2026-03-04 17:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 52.6 MB (95.07%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,34 +1,55 @@
+// class Solution {
+//     public int singleNonDuplicate(int[] arr) {
+//         int n = arr.length; 
+//         int lo = 0, hi = n - 1;
+
+//         if(n == 1) return arr[0];
+//         if(arr[0] != arr[1]) return arr[0];
+//         if(arr[n-1] != arr[n-2]) return arr[n-1];
+
+//         while(lo <= hi){
+//             int mid = lo + (hi - lo) / 2;
+
+//             if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) 
+//                 return arr[mid];
+
+//             int f = mid, s = mid;
+
+//             if(arr[mid] == arr[mid-1]) 
+//                 f = mid - 1;
+//             else 
+//                 s = mid + 1;
+
+//             int leftCount = f - lo;
+//             int rightCount = hi - s;
+
+//             if(leftCount % 2 == 0)
+//                 lo = s + 1;
+//             else
+//                 hi = f - 1;
+//         }
+
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int singleNonDuplicate(int[] arr) {
-        int n = arr.length; 
-        int lo = 0, hi = n - 1;
 
-        if(n == 1) return arr[0];
-        if(arr[0] != arr[1]) return arr[0];
-        if(arr[n-1] != arr[n-2]) return arr[n-1];
+        int lo = 0;
+        int hi = arr.length - 1;
 
-        while(lo <= hi){
-            int mid = lo + (hi - lo) / 2;
+        while(lo < hi){
+            int mid = (lo + hi) / 2;
 
-            if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) 
-                return arr[mid];
+            if(mid % 2 == 1) mid--;  
 
-            int f = mid, s = mid;
-
-            if(arr[mid] == arr[mid-1]) 
-                f = mid - 1;
-            else 
-                s = mid + 1;
-
-            int leftCount = f - lo;
-            int rightCount = hi - s;
-
-            if(leftCount % 2 == 0)
-                lo = s + 1;
+            if(arr[mid] == arr[mid + 1])
+                lo = mid + 2;
             else
-                hi = f - 1;
+                hi = mid;
         }
 
-        return -1;
+        return arr[lo];
     }
 }
