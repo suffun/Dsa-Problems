543eec16f69504440c862f2c6ed61e2859dae0dd
sufiyan khan <160502499+suffun@users.noreply.github.com>
2026-05-18 20:01:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (8.32%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -2,39 +2,42 @@ class Solution {
     public List<Integer> spiralOrder(int[][] arr) {
         List<Integer> ans = new ArrayList<>();
 
-        int m = arr.length, n = arr[0].length;
+        int m = arr.length;
+        int n = arr[0].length;
+
         int firstRow = 0, lastRow = m - 1;
         int firstCol = 0, lastCol = n - 1;
 
-        while (firstRow <= lastRow && firstCol <= lastCol) {
+        int total = m * n;
 
-            // Top row
+        while (ans.size() < total) {
+
+            // → Traverse right (top row)
             for (int j = firstCol; j <= lastCol; j++) {
                 ans.add(arr[firstRow][j]);
             }
             firstRow++;
+            if (ans.size() >= total) break;
 
-            // Right column
+            // ↓ Traverse down (right column)
             for (int i = firstRow; i <= lastRow; i++) {
                 ans.add(arr[i][lastCol]);
             }
             lastCol--;
+            if (ans.size() >= total) break;
 
-            // Bottom row
-            if (firstRow <= lastRow) {
-                for (int j = lastCol; j >= firstCol; j--) {
-                    ans.add(arr[lastRow][j]);
-                }
-                lastRow--;
+            // ← Traverse left (bottom row)
+            for (int j = lastCol; j >= firstCol; j--) {
+                ans.add(arr[lastRow][j]);
             }
+            lastRow--;
+            if (ans.size() >= total) break;
 
-            // Left column
-            if (firstCol <= lastCol) {
-                for (int i = lastRow; i >= firstRow; i--) {
-                    ans.add(arr[i][firstCol]);
-                }
-                firstCol++;
+            // ↑ Traverse up (left column)
+            for (int i = lastRow; i >= firstRow; i--) {
+                ans.add(arr[i][firstCol]);
             }
+            firstCol++;
         }
 
         return ans;
