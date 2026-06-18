053d8c44ce9ad25e70c8aa1c52b044d1cab5cb06
sufiyan khan <160502499+suffun@users.noreply.github.com>
2026-06-18 21:35:09 +0530

    Time: 5 ms (44.03%) | Memory: 94.3 MB (78.39%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+       static ListNode reverse(ListNode head2){
+       ListNode curr = head2;
+       ListNode prev = null;
+       ListNode fwd = null;
+       while(curr != null){
+        fwd = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = fwd;
+       }
+       return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+
+       ListNode slow = head;
+       ListNode fast = head;
+       while(fast.next != null && fast.next.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+       }
+       ListNode head2 = slow.next;
+       slow.next = null;
+
+       head2 = reverse(head2);
+
+        ListNode i = head;
+        ListNode j = head2;
+        while(j != null){
+            if(i.val != j.val) return false;
+            i = i.next;
+            j = j.next;
+        }
+        return true;
+
+       }
+       
+
+
+
+
+    //  public boolean isPalindrome(ListNode head) {
+     //    ArrayList<Integer> arr = new ArrayList<>();
+    //    ListNode temp = head;
+    //    while(temp != null){
+    //     arr.add(temp.val);
+    //     temp = temp.next;
+
+    //    } 
+
+    //    int i = 0, j = arr.size() - 1;
+    //    while(i<=j){
+    //     int a = arr.get(i);
+    //     int b = arr.get(j);
+    //     if(a != b) return false;
+    //         i++;
+    //         j--;
+
+    //    }
+    //    return true;
+    
+        
+    // }
+}
