93b8f4c3b7d6073c189159e7403c29290aa05e7e
sufiyan khan <160502499+suffun@users.noreply.github.com>
2026-07-02 18:16:58 +0530

    Time: 2 ms (10.60%) | Memory: 42.9 MB (12.25%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,4 +1,7 @@
 class MyQueue {
+
+    // pop efficient approach
+
     Stack<Integer> st = new Stack<>();
     Stack<Integer> helper = new Stack<>();
 
@@ -6,33 +9,28 @@ class MyQueue {
         
     }
     
-    public void push(int x) {
-        st.push(x);
+    public void push(int x) { //O(n)
+    // Push at bottom of st
+
+    while(st.size()>0){
+        helper.push(st.pop());
+    }
+    st.push(x);
+      while(helper.size()>0){
+        st.push(helper.pop());
+    }
+   
+   
         
     }
     
-    public int pop() {
-        while(st.size()>1){
-            helper.push(st.pop());
-        }
-        int front = st.pop();
-        while(helper.size()>0){
-            st.push(helper.pop());
-        }
-        return front;
+    public int pop() {  //O(1)
+        return st.pop();
 
     }
     
-    public int peek() {
-          while(st.size()>1){
-            helper.push(st.pop());
-        }
-        int front = st.peek();
-        while(helper.size()>0){
-            st.push(helper.pop());
-        }
-        return front;
-    
+    public int peek() {  //O(1)
+        return st.peek();
         
     }
     
@@ -40,6 +38,51 @@ class MyQueue {
         return (st.size()==0);
     }
 }
+
+
+
+
+//     Stack<Integer> st = new Stack<>();
+//     Stack<Integer> helper = new Stack<>();
+
+//     public MyQueue() {
+        
+//     }
+    
+//     public void push(int x) {
+//         st.push(x);
+        
+//     }
+    
+//     public int pop() {
+//         while(st.size()>1){
+//             helper.push(st.pop());
+//         }
+//         int front = st.pop();
+//         while(helper.size()>0){
+//             st.push(helper.pop());
+//         }
+//         return front;
+
+//     }
+    
+//     public int peek() {
+//           while(st.size()>1){
+//             helper.push(st.pop());
+//         }
+//         int front = st.peek();
+//         while(helper.size()>0){
+//             st.push(helper.pop());
+//         }
+//         return front;
+    
+        
+//     }
+    
+//     public boolean empty() {
+//         return (st.size()==0);
+//     }
+// }
 
 /**
  * Your MyQueue object will be instantiated and called as such:
