f6f6520d6669da78b20886b03e68f34e7750bf51
sufiyan khan <160502499+suffun@users.noreply.github.com>
2026-08-08 08:48:39 +0530

    Time: 15 ms (91.45%) | Memory: 70 MB (35.96%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,25 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root==null) return null;
+//         if(root.val == p.val || root.val == q.val) return root;
+//         boolean pLiesInLeft = exists(root.left,p);
+//         boolean qLiesInRight = exists(root.left,q);
+//         if(pLiesInLeft && !qLiesInRight) return  lowestCommonAncestor(root.left,p,q);
+//         else if(!pLiesInLeft && qLiesInRight) return  lowestCommonAncestor(root.right,p,q);
+//         else return root;
+//     }
+//     public boolean exists(TreeNode root, TreeNode target){
+//         if(root == null) return false;
+//         if(root.val == target.val) return true;
+//         return exists(root.left,target) || exists(root.right,target);
+//     }
+// }
+
+
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null) return null;
         if(root.val == p.val || root.val == q.val) return root;
-        boolean pLiesInLeft = exists(root.left,p);
-        boolean qLiesInLeft = exists(root.left,q);
-        if(pLiesInLeft && qLiesInLeft) return  lowestCommonAncestor(root.left,p,q);
-        else if(!pLiesInLeft && !qLiesInLeft) return  lowestCommonAncestor(root.right,p,q);
-        else return root;
+        TreeNode l = lowestCommonAncestor(root.left,p,q);
+        TreeNode r = lowestCommonAncestor(root.right,p,q);
+        if(l!=null && r!= null) return root;
+        return(l==null) ? r : l ;
     }
-    public boolean exists(TreeNode root, TreeNode target){
-        if(root == null) return false;
-        if(root.val == target.val) return true;
-        return exists(root.left,target) || exists(root.right,target);
-    }
+    
 }
