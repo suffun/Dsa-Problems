2543a00c121be3cc058040f6425e59e643290da1
sufiyan khan <160502499+suffun@users.noreply.github.com>
2026-08-14 20:33:00 +0530

    Time: 1 ms (19.32%) | Memory: 44.3 MB (16.92%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,25 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
     public void flatten(TreeNode root) {
+        if (root == null) return;
+        ArrayList<TreeNode> arr = new ArrayList<>();
+        dfs(root,arr);
+        for(int i = 0; i<arr.size()-1;i++){
+            TreeNode a = arr.get(i);
+            TreeNode b = arr.get(i+1); 
+            a.right = b;
+            a.left = null;
+        }
+        TreeNode last = arr.get(arr.size() - 1);
+        last.left = null;
+        last.right = null;
 
-        if (root == null)
-            return;
-
-        TreeNode lst = root.left;
-        TreeNode rst = root.right;
-
-        root.left = null;
-
-        flatten(lst);
-        flatten(rst);
-
-        root.right = lst;
-
-        TreeNode last = root;
-
-        while (last.right != null)
-            last = last.right;
-
-        last.right = rst;
+    }
+    public void dfs(TreeNode root, ArrayList<TreeNode> arr){
+        if(root == null) return;
+        arr.add(root);
+        dfs(root.left, arr);
+        dfs(root.right, arr);
+        
     }
 }
