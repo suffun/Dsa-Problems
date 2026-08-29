8b113d1c82673b894139ebe4ed7772b58bb1f031
sufiyan khan <160502499+suffun@users.noreply.github.com>
2026-08-29 11:01:08 +0530

    Time: 1 ms (29.58%) | Memory: 47.2 MB (82.41%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,27 +1,41 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+
+//         int n = nums.length;
+
+//         int expectedSum = n * (n + 1) / 2;
+
+//         int actualSum = 0;
+
+//         for (int num : nums) {
+//             actualSum += num;
+//         }
+
+//         return expectedSum - actualSum;
+//     }
+// }
+
 class Solution {
     public int missingNumber(int[] arr) {
         int n = arr.length;
         int i = 0;
-        while(i< arr.length){
-            if(arr[i] == i|| arr[i] ==n) i++;
+        while(i<n){
+
+            if(arr[i]==i || arr[i] ==n) i++;
             else{
                 int idx = arr[i];
                 swap(arr,i,idx);
             }
-            
         }
-        for(i = 0; i<arr.length; i++){
+        for( i = 0; i<n; i++){
             if(arr[i] != i) return i;
-            
         }
         return n;
-        
-        
     }
-    private void swap(int[] arr, int i, int idx){
-            int temp = arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = temp;
-        
+    public void swap(int[] arr, int i, int idx){
+        int temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
+
     }
 }
