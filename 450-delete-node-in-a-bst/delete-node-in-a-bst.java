f149e68e99792bf2bc85019f052ba5f0dc7cc38e
sufiyan khan <160502499+suffun@users.noreply.github.com>
2026-09-17 23:44:36 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (18.48%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+        if(root.val > key) root.left = deleteNode(root.left,key);
+        else if(root.val < key) root.right = deleteNode(root.right,key);
+        else{
+            if(root.left == null && root.right == null) return null;
+
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+
+            TreeNode pred = root.left;
+            while(pred.right != null){
+                pred = pred.right;
+            }
+            root.left = deleteNode(root.left,pred.val);
+            pred.left = root.left;
+            pred.right = root.right;
+            return pred;
+        }
+        return root;
+    }
+}
